00f950a8cbb70708d76025d8fdd1187b45e66b21
Administrator <252319634@qq.com>
2016-02-23 11:22:29 +0800

    基本完善

--- store/wsgi.py
+++ store/wsgi.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-root = os.path.dirname(__file__)                                             #输出当前文件所在文件夹的绝对路径
-sys.path.insert(0, os.path.join(root, '..', 'site-packages'))                #将root路径中的上级目录的site-packages加入到Path中去
+root = os.path.dirname(__file__)
+# 输出当前文件所在文件夹的绝对路径
+sys.path.insert(0, os.path.join(root, '..', 'site-packages'))
+# 将root路径中的上级目录的site-packages加入到Path中去
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
-
 
 application = get_wsgi_application()
